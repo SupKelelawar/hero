4f9c33b2921f5dfabe9d98bdd24683681bea7ded
SupKelelawar <138984997+SupKelelawar@users.noreply.github.com>
2024-11-10 13:11:26 +0700

    Update pick.js

--- util/pick.js
+++ util/pick.js
@@ -1,18 +1,18 @@
 // Since only single function required from lodash or underscore, writing it self
 
 module.exports = (obj, keys) => {
-	const new_obj = {};
+        const new_obj = {};
 
-	if(!obj)	obj={};
-	if( ! Array.isArray(keys) ){
-		keys = [keys];	// convert to array
-	}
+        if(!obj)        obj={};
+        if( ! Array.isArray(keys) ){
+                keys = [keys];        // convert to array
+        }
 
-	for (const key in obj) {
-		if (Object.hasOwnProperty.call(obj, key) && keys.includes(key)) {
-			new_obj[key] = obj[key];
-		}
-	}
+        for (const key in obj) {
+                if (Object.hasOwnProperty.call(obj, key) && keys.includes(key)) {
+                        new_obj[key] = obj[key];
+                }
+        }
 
-	return new_obj;
+        return new_obj;
 };
